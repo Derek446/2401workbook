33a2637d57de49cafde34e12ff74d1ca7506b4c6
Derek <derek358@icloud.com>
2026-02-04 21:52:42 -0700

    Started 10

--- 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
+++ 10-orm-views-with-models-continued-start/mysoftwarecompany/clients/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import Company
+from .models import *
 
 
 def list_companies(request):
@@ -9,3 +9,22 @@ def list_companies(request):
     companies = Company.objects.all()
 
     return render(request, 'clients/companies_list.html', {'companies': companies})
+
+def company_detail(request, company_id):
+    company = get_object_or_404(Company, id=company_id)
+
+    return render(request, 'clients/company_detail.html', {'company': company})
+
+def employee_search_results(request, company_id):
+    # 'q' will be in the url
+    query = request.GET.get('q', '')
+    company = get_object_or_404(Company, id=company_id)
+
+    if query:
+        employees = company.employees.filter(
+            first_name__icontains=query
+        )
+    else:
+        employees = Employee.objects.none()
+
+    return render(request, 'clients/employee_search_results.html', {'employees': employees, 'query': query})
--- 10-orm-views-with-models-continued-start/mysoftwarecompany/mysoftwarecompany/settings.py
+++ 10-orm-views-with-models-continued-start/mysoftwarecompany/mysoftwarecompany/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "mysoftwarecompany.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mysoftwarecompany_new",
+        "USER": "dbadmin",
+        "PASSWORD": "dbadmin",
+        "HOST": "localhost", # if left blank, will default to localhost
+        "PORT": "5432", # if left blank, will default to 5432
     }
 }
 
